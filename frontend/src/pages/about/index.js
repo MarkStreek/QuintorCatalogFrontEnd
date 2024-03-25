1ee5c972f7d0d79a2c3db0b46a24afd7ee14269b
MarkStreek <m.van.de.streek@st.hanze.nl>
2024-03-25 14:30:56 +0100

    Created a custom useHook functions and moved the devices table to a different page

---
+++ frontend/src/pages/about/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import RootLayout from "@/app/components/RootLayout";
+export default function Index() {
+    return(
+        <RootLayout>
+            <p className="text-4xl">About page</p>
+            <br />
+            <p>This application...</p>
+            <br />
+            <p className="text-2xl">Info about adding Devices</p>
+        </RootLayout>
+    );
+}
