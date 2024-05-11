1f3152d6646e1acfcf82e3100d0f5674e48f4215
MarkStreek <m.van.de.streek@st.hanze.nl>
2024-05-11 23:04:27 +0200

    Removed the postNewDevice function, because it's not being used anymore

--- frontend/src/pages/addDevice/SaveDevice.js
+++ frontend/src/pages/addDevice/SaveDevice.js
@@ -14,31 +14,6 @@ export const translationMap = {
 };
 
 /**
- * Async function that sends a POST request to the API to add a new device.
- * The device data is stored in the DeviceData state and used for the fetch request.
- */
-export default async function POSTnewDevice(DeviceData) {
-    console.log(JSON.stringify(DeviceData));
-    const translatedData = translateKeys(DeviceData, translationMap);
-    const requestOptions = {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(translatedData)
-    };
-    try {
-        let response = await fetch('http://localhost:8080/devices', requestOptions)
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-    } catch (error) {
-        console.error('Error submitting form:', error);
-        alert(error);
-    }
-
-
-}
-
-/**
  * Function that translates the keys of an object using a translation map.
  * This because the backend needs different keys than the frontend.
  *
