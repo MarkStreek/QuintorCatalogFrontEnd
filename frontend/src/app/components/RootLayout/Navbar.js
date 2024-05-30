46aec2f08e47fc4c3d3542b4916031ef4b6f8429
MarkStreek <m.van.de.streek@st.hanze.nl>
2024-05-30 11:38:40 +0200

    Created a dropdown menu for smaller screens.

--- frontend/src/app/components/RootLayout/Navbar.js
+++ frontend/src/app/components/RootLayout/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Link from "next/link";
+import { Dropdown, DropdownItem, DropdownTrigger, DropdownMenu, Button } from "@nextui-org/react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 /**
  * Function that returns the Navbar component.
@@ -11,26 +13,42 @@ import Link from "next/link";
  */
 export default function Navbar() {
     return (
-        <div className="bg-quintor-red text-gray-800 w-full xs:flex sm:flex md:hidden h-16 items-center justify-between px-2 fixed top-0 left-0 z-10">
+        <div className="bg-quintor-red text-gray-300 w-full xs:flex sm:flex md:hidden h-16 items-center justify-between px-2 fixed top-0 left-0 z-10">
             <Link href="/"><span className="text-2xl font-extrabold">Quintor Catalogus</span></Link>
             <nav>
-                <ul className="flex space-x-3">
-                    <li>
-                        <Link href="/devices"
-                              className="text-gray-800 hover:bg-gray-700 hover:text-white px-2 py-3 rounded-md">Apparaten</Link>
-                    </li>
-                    <li>
-                        <Link href="/addDevice"
-                              className="text-gray-800 hover:bg-gray-700 hover:text-white px-2 py-3 rounded-md">Apparaten toevoegen</Link>
-                    </li>
-                    <li>
-                        <Link href="/borrowedStatus"
-                              className="text-gray-300 hover:bg-gray-700 hover:text-white px-2 py-3 rounded-md">Uitleenstatus</Link>
-                    </li>
-                    <li>
+                <ul className="flex">
+                    <li className="mt-2 mr-3">
                         <Link href="/about"
-                              className="text-gray-800 hover:bg-gray-700 hover:text-white px-2 py-3 rounded-md">Over</Link>
+                              className="text-gray-300 hover:bg-gray-700 hover:text-white px-2 py-3 rounded-md">
+                            Over
+                        </Link>
                     </li>
+                    <Dropdown>
+                        <DropdownTrigger>
+                            <Button
+                                variant="faded"
+                            >
+                                <GiHamburgerMenu/>
+                            </Button>
+                        </DropdownTrigger>
+                        <DropdownMenu aria-label="Static Actions">
+                            <DropdownItem key="devices">
+                                <Link href="/devices" className="text-gray-800 px-2 py-3 rounded-md">
+                                    Apparaten
+                                </Link>
+                            </DropdownItem>
+                            <DropdownItem key="addDevice">
+                                <Link href="/addDevice" className="text-gray-800 px-2 py-3 rounded-md">
+                                    Apparaten toevoegen
+                                </Link>
+                            </DropdownItem>
+                            <DropdownItem key="borrowedStatus">
+                                <Link href="/borrowedStatus" className="text-gray-800 px-2 py-3 rounded-md">
+                                    Uitleenstatus
+                                </Link>
+                            </DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
                 </ul>
             </nav>
         </div>
--- frontend/src/app/components/RootLayout/Sidebar.js
+++ frontend/src/app/components/RootLayout/Sidebar.js
@@ -11,7 +11,7 @@ import Link from "next/link";
  */
 export default function Sidebar() {
     return (
-        <div className="h-screen bg-quintor-red text-gray-800 w-48 space-y-6 py-7 px-2 fixed inset-y-0 left-0 overflow-auto">
+        <div className="h-screen bg-quintor-red text-gray-300 w-48 space-y-6 py-7 px-2 fixed inset-y-0 left-0 overflow-auto">
             <div className="flex items-center space-x-4">
                 <Link href="/"><span className="text-2xl font-extrabold">Quintor Catalogus</span></Link>
             </div>
@@ -19,15 +19,15 @@ export default function Sidebar() {
                 <ul className="space-y-3">
                     <li>
                         <Link href="/"
-                              className="text-gray-800 hover:bg-gray-700 hover:text-white px-2 py-3 rounded-md">Home</Link>
+                              className="text-gray-300 hover:bg-gray-700 hover:text-white px-2 py-3 rounded-md">Home</Link>
                     </li>
                     <li>
                         <Link href="/devices"
-                              className="text-gray-800 hover:bg-gray-700 hover:text-white px-2 py-3 rounded-md">Apparaten</Link>
+                              className="text-gray-300 hover:bg-gray-700 hover:text-white px-2 py-3 rounded-md">Apparaten</Link>
                     </li>
                     <li>
                         <Link href="/addDevice"
-                              className="text-gray-800 hover:bg-gray-700 hover:text-white px-2 py-3 rounded-md">Apparaten toevoegen</Link>
+                              className="text-gray-300 hover:bg-gray-700 hover:text-white px-2 py-3 rounded-md">Apparaten toevoegen</Link>
                     </li>
                     <li>
                         <Link href="/borrowedStatus"
@@ -35,7 +35,7 @@ export default function Sidebar() {
                     </li>
                     <li>
                         <Link href="/about"
-                              className="text-gray-800 hover:bg-gray-700 hover:text-white px-2 py-3 rounded-md">Over</Link>
+                              className="text-gray-300 hover:bg-gray-700 hover:text-white px-2 py-3 rounded-md">Over</Link>
                     </li>
                 </ul>
             </nav>
